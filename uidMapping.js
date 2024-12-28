6e5db3f7500603a72d0d1a3cee7e92a66b87ab5e
Bye-lo <60986485+Bye-lo@users.noreply.github.com>
2024-12-28 14:03:38 +0800

    Add files via upload

---
+++ uidMapping.js
@@ -0,0 +1,7 @@
+const uidMap = {
+    '261080295': 'pages/underdog的年度总结.html',
+    '224114431': 'pages/司命的年度总结.html',
+    '130294434': 'pages/司命☁的年度总结.html',
+    'pixiaodan': 'pages/六扇门内李银心的年度总结.html',
+    '189480748': 'pages/Nothing的年度总结.html'
+};
